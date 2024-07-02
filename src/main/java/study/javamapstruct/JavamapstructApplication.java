b15c3e958086dbf44f7d4f6b8cfe56085e189a4b
juhyeon-60hz <jh.kim1114@60hz.io>
2024-07-02 16:24:44 +0900

    init

---
+++ src/main/java/study/javamapstruct/JavamapstructApplication.java
@@ -0,0 +1,13 @@
+package study.javamapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamapstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/javamapstruct/JavamapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package study.javamapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
